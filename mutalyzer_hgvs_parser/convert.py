8c7f03810a282d55472cb8480f7f2a01c9c2a668
mihailefter <mihai.lefter@gmail.com>
2020-09-24 16:57:58 +0200

    Do not include errors in the model (#24)

--- mutalyzer_hgvs_parser/convert.py
+++ mutalyzer_hgvs_parser/convert.py
@@ -4,6 +4,7 @@ Module for converting lark parse trees to their equivalent dictionary models.
 
 from lark import Tree
 from lark.lexer import Token
+
 from .exceptions import UnsupportedStartRule
 
 
@@ -13,13 +14,13 @@ def to_model(parse_tree, start_rule=None):
     """
     if start_rule is None:
         return description_to_model(parse_tree)
-    if start_rule == 'reference':
+    if start_rule == "reference":
         return _reference_to_model(parse_tree)
-    elif start_rule == 'variants':
+    elif start_rule == "variants":
         return _variants_to_model(parse_tree)
-    elif start_rule == 'variant':
+    elif start_rule == "variant":
         return _variant_to_model(parse_tree)
-    elif start_rule == 'location':
+    elif start_rule == "location":
         return _location_to_model(parse_tree)
     raise UnsupportedStartRule(start_rule)
 
@@ -36,13 +37,13 @@ def description_to_model(parse_tree):
     if isinstance(parse_tree, Tree):
         for child in parse_tree.children:
             if isinstance(child, Token):
-                if child.type == 'COORDINATE_SYSTEM':
-                    model['coordinate_system'] = child.value
+                if child.type == "COORDINATE_SYSTEM":
+                    model["coordinate_system"] = child.value
             elif isinstance(parse_tree, Tree):
-                if child.data == 'reference':
-                    model['reference'] = _reference_to_model(child)
-                elif child.data == 'variants':
-                    model['variants'] = _variants_to_model(child)
+                if child.data == "reference":
+                    model["reference"] = _reference_to_model(child)
+                elif child.data == "variants":
+                    model["variants"] = _variants_to_model(child)
     return model
 
 
@@ -55,10 +56,12 @@ def _reference_to_model(reference_tree):
     :return: Dictionary model.
     """
     if len(reference_tree.children) == 1:
-        return {'id': reference_tree.children[0].value}
+        return {"id": reference_tree.children[0].value}
     elif len(reference_tree.children) == 2:
-        return {'id': reference_tree.children[0].value,
-                'selector': _reference_to_model(reference_tree.children[1])}
+        return {
+            "id": reference_tree.children[0].value,
+            "selector": _reference_to_model(reference_tree.children[1]),
+        }
 
 
 def _variants_to_model(variants_tree):
@@ -83,27 +86,24 @@ def _variant_to_model(variant_tree):
     :param variant_tree: Lark parse tree.
     :return: Dictionary model.
     """
-    if variant_tree.data == '_ambig':
+    if variant_tree.data == "_ambig":
         variant_tree = _solve_del_variant_ambiguity(variant_tree)
 
-    variant = {'location': _location_to_model(
-        variant_tree.children[0])}
+    variant = {"location": _location_to_model(variant_tree.children[0])}
     if len(variant_tree.children) == 2:
         variant_tree = variant_tree.children[1]
-        variant['type'] = variant_tree.data
-        variant['source'] = 'reference'
-        if variant_tree.data == 'repeat':
-            variant['inserted'] = _repeat_to_model(variant_tree.children)
+        variant["type"] = variant_tree.data
+        variant["source"] = "reference"
+        if variant_tree.data == "repeat":
+            variant["inserted"] = _repeat_to_model(variant_tree.children)
         elif len(variant_tree.children) == 1:
-            if variant_tree.data == 'deletion':
-                variant['deleted'] = _deleted_to_model(
-                    variant_tree.children[0])
+            if variant_tree.data == "deletion":
+                variant["deleted"] = _deleted_to_model(variant_tree.children[0])
             else:
-                variant['inserted'] = _inserted_to_model(
-                    variant_tree.children[0])
+                variant["inserted"] = _inserted_to_model(variant_tree.children[0])
         elif len(variant_tree.children) == 2:
-            variant['deleted'] = _deleted_to_model(variant_tree.children[0])
-            variant['inserted'] = _inserted_to_model(variant_tree.children[1])
+            variant["deleted"] = _deleted_to_model(variant_tree.children[0])
+            variant["inserted"] = _inserted_to_model(variant_tree.children[1])
 
     return variant
 
@@ -116,11 +116,15 @@ def _solve_del_variant_ambiguity(variant):
     wrongly interpreted as a reference, or as a deletion insertion, case
     in which REF2 is the reference (correct).
     """
-    if variant.children[0].children[1].data == 'deletion_insertion' \
-            and variant.children[1].children[1].data == 'deletion':
+    if (
+        variant.children[0].children[1].data == "deletion_insertion"
+        and variant.children[1].children[1].data == "deletion"
+    ):
         return variant.children[0]
-    elif variant.children[1].children[1].data == 'deletion_insertion' \
-            and variant.children[0].children[1].data == 'deletion':
+    elif (
+        variant.children[1].children[1].data == "deletion_insertion"
+        and variant.children[0].children[1].data == "deletion"
+    ):
         return variant.children[1]
 
 
@@ -133,9 +137,9 @@ def _location_to_model(location_tree):
     :return: Dictionary model.
     """
     location_tree = location_tree.children[0]
-    if location_tree.data == 'range':
+    if location_tree.data == "range":
         return _range_to_model(location_tree)
-    elif location_tree.data in ['point', 'uncertain_point']:
+    elif location_tree.data in ["point", "uncertain_point"]:
         return _point_to_model(location_tree)
 
 
@@ -147,11 +151,11 @@ def _range_to_model(range_tree):
     :param range_tree:
     :return:
     """
-    range_location = {'type': 'range',
-                      'start': _point_to_model(
-                          range_tree.children[0]),
-                      'end': _point_to_model(
-                          range_tree.children[1])}
+    range_location = {
+        "type": "range",
+        "start": _point_to_model(range_tree.children[0]),
+        "end": _point_to_model(range_tree.children[1]),
+    }
     return range_location
 
 
@@ -163,29 +167,28 @@ def _point_to_model(point_tree):
     :param point_tree: Lark parse tree.
     :return: Dictionary model.
     """
-    if point_tree.data == 'uncertain_point':
-        return {**_range_to_model(point_tree),
-                **{'uncertain': True}}
-    point = {'type': 'point'}
+    if point_tree.data == "uncertain_point":
+        return {**_range_to_model(point_tree), **{"uncertain": True}}
+    point = {"type": "point"}
     for token in point_tree.children:
-        if token.type == 'OUTSIDE_CDS':
-            if token.value == '*':
-                point['outside_cds'] = 'downstream'
-            elif token.value == '-':
-                point['outside_cds'] = 'upstream'
-        elif token.type == 'NUMBER':
-            point['position'] = int(token.value)
-        elif token.type == 'UNKNOWN':
-            point['uncertain'] = True
-        elif token.type == 'OFFSET':
-            if '?' in token.value:
-                point['offset'] = {'uncertain': True}
-                if '+' in token.value:
-                    point['offset']['downstream'] = True
-                elif '-' in token.value:
-                    point['offset']['upstream'] = True
+        if token.type == "OUTSIDE_CDS":
+            if token.value == "*":
+                point["outside_cds"] = "downstream"
+            elif token.value == "-":
+                point["outside_cds"] = "upstream"
+        elif token.type == "NUMBER":
+            point["position"] = int(token.value)
+        elif token.type == "UNKNOWN":
+            point["uncertain"] = True
+        elif token.type == "OFFSET":
+            if "?" in token.value:
+                point["offset"] = {"uncertain": True}
+                if "+" in token.value:
+                    point["offset"]["downstream"] = True
+                elif "-" in token.value:
+                    point["offset"]["upstream"] = True
             else:
-                point['offset'] = {'value': int(token.value)}
+                point["offset"] = {"value": int(token.value)}
     return point
 
 
@@ -204,9 +207,9 @@ def _repeat_to_model(repeats):
             inserted = {}
             make_new_inserted = False
         if isinstance(repeat, Token):
-            inserted['sequence'] = repeat.value
+            inserted["sequence"] = repeat.value
         elif isinstance(repeat, Tree):
-            inserted['length'] = _length_to_model(repeat)
+            inserted["length"] = _length_to_model(repeat)
             model_repeats.append(inserted)
             make_new_inserted = True
     return model_repeats
@@ -223,11 +226,13 @@ def _length_to_model(length_tree):
     length_tree = length_tree.children[0]
     if isinstance(length_tree, Token):
         return _length_point_to_model(length_tree)
-    elif length_tree.data == 'exact_range':
-        return {'type': 'range',
-                'start': _length_point_to_model(length_tree.children[0]),
-                'end': _length_point_to_model(length_tree.children[1]),
-                'uncertain': True}
+    elif length_tree.data == "exact_range":
+        return {
+            "type": "range",
+            "start": _length_point_to_model(length_tree.children[0]),
+            "end": _length_point_to_model(length_tree.children[1]),
+            "uncertain": True,
+        }
 
 
 def _length_point_to_model(length_point_token):
@@ -238,12 +243,10 @@ def _length_point_to_model(length_point_token):
     :param length_point_token: Lark token.
     :return: Dictionary model.
     """
-    if length_point_token.type == 'UNKNOWN':
-        return {'type': 'point',
-                'uncertain': True}
-    if length_point_token.type == 'NUMBER':
-        return {'type': 'point',
-                'value': int(length_point_token.value)}
+    if length_point_token.type == "UNKNOWN":
+        return {"type": "point", "uncertain": True}
+    if length_point_token.type == "NUMBER":
+        return {"type": "point", "value": int(length_point_token.value)}
 
 
 def _deleted_to_model(deleted):
@@ -255,8 +258,7 @@ def _deleted_to_model(deleted):
     :return:
     """
     if isinstance(deleted, Token):
-        return [{'sequence': deleted.value,
-                'source': 'description'}]
+        return [{"sequence": deleted.value, "source": "description"}]
     return _inserted_to_model(deleted)
 
 
@@ -271,40 +273,41 @@ def _inserted_to_model(inserted_tree):
     inserted = []
     for inserted_subtree in inserted_tree.children:
         insert = {}
-        if inserted_subtree.data == '_ambig':
+        if inserted_subtree.data == "_ambig":
             inserted_subtree = _solve_insert_ambiguity(inserted_subtree)
         for insert_part in inserted_subtree.children:
             if isinstance(insert_part, Token):
-                if insert_part.type == 'SEQUENCE':
-                    insert.update({'sequence': insert_part.value,
-                                   'source': 'description'})
-                elif insert_part.type == 'INVERTED':
-                    insert['inverted'] = True
+                if insert_part.type == "SEQUENCE":
+                    insert.update(
+                        {"sequence": insert_part.value, "source": "description"}
+                    )
+                elif insert_part.type == "INVERTED":
+                    insert["inverted"] = True
             elif isinstance(insert_part, Tree):
-                if insert_part.data == 'location':
-                    insert['location'] = _location_to_model(insert_part)
-                    insert['source'] = 'reference'
-                elif insert_part.data == 'length':
-                    insert['length'] = _length_to_model(insert_part)
-                elif insert_part.data == 'description':
+                if insert_part.data == "location":
+                    insert["location"] = _location_to_model(insert_part)
+                    insert["source"] = "reference"
+                elif insert_part.data == "length":
+                    insert["length"] = _length_to_model(insert_part)
+                elif insert_part.data == "description":
                     for description_part in insert_part.children:
-                        if isinstance(description_part, Token) and \
-                                description_part.type == 'COORDINATE_SYSTEM':
-                            insert['coordinate_system'] = \
-                                description_part.value
-                        elif description_part.data == 'variants':
+                        if (
+                            isinstance(description_part, Token)
+                            and description_part.type == "COORDINATE_SYSTEM"
+                        ):
+                            insert["coordinate_system"] = description_part.value
+                        elif description_part.data == "variants":
                             if len(description_part.children) != 1:
-                                raise Exception('Nested descriptions?')
+                                raise Exception("Nested descriptions?")
                             variant = description_part.children[0]
                             if len(variant.children) != 1:
-                                raise Exception('Nested descriptions?')
+                                raise Exception("Nested descriptions?")
                             else:
-                                insert['location'] = \
-                                    _location_to_model(
-                                        variant.children[0])
-                        elif description_part.data == 'reference':
-                            insert['source'] = _reference_to_model(
-                                description_part)
+                                insert["location"] = _location_to_model(
+                                    variant.children[0]
+                                )
+                        elif description_part.data == "reference":
+                            insert["source"] = _reference_to_model(description_part)
         inserted.append(insert)
     return inserted
 
@@ -339,9 +342,13 @@ def _solve_insert_ambiguity(insert):
     - (10_20)_(30_40)
     """
     if len(insert.children) == 2:
-        if insert.children[0].children[0].data == 'length' and \
-                insert.children[1].children[0].data == 'location':
+        if (
+            insert.children[0].children[0].data == "length"
+            and insert.children[1].children[0].data == "location"
+        ):
             return insert.children[0]
-        elif insert.children[0].children[0].data == 'location' and \
-                insert.children[1].children[0].data == 'length':
+        elif (
+            insert.children[0].children[0].data == "location"
+            and insert.children[1].children[0].data == "length"
+        ):
             return insert.children[1]
